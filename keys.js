4b20370017ae7433e3d711b305c9a799e6424c8e
Shivam Sansare <shivam.sansare99@gmail.com>
2020-05-28 20:54:43 +0530

    first commit

--- keys.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    api:{
-        gmail:"sansaredevelopers@gmail.com",
-        password:"Sansare@99",
-        mongoDb:"mongodb+srv://shivam:chatbot@cluster0-toe29.mongodb.net/chatbot?retryWrites=true&w=majority        ",
-        weather : "4a986ddaed3d123a22e010b0d6f7f126",
-    }
-};
