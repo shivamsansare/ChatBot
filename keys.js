ce5b4373e7df97f75c2685f9c0577fd571c06caa
Shivam Sansare <shivam.sansare99@gmail.com>
2020-05-28 12:07:35 +0530

    first commit

--- keys.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    api:{
-        mongoDb:"mongodb+srv://shivam:chatbot@cluster0-toe29.mongodb.net/chatbot?retryWrites=true&w=majority        ",
-        weather : "4a986ddaed3d123a22e010b0d6f7f126",
-    }
-};
